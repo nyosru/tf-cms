3b76fa48e91a74cfa6c5d42f8f06a8280edbcea7
Sergey <1@php-cat.com>
2024-12-21 04:05:59 +0500

    добавил шапку в бух заказы

--- resources/views/livewire/cms2/buh-zakaz.blade.php
+++ resources/views/livewire/cms2/buh-zakaz.blade.php
@@ -1,13 +1,35 @@
 <div>
 
+    <div class="app-content-header"> <!--begin::Container-->
+        <div class="container-fluid"> <!--begin::Row-->
+            <div class="row">
+                <div class="col-sm-4">
+                    <h3 class="mb-0">Заказы</h3>
+                </div>
+                <div class="col-sm-4">
+                    <input type="text" wire:model.live="searchTerm" placeholder="Поиск по клиенту ..."
+                           class="form-control">
+                </div>
+                <div class="col-sm-4">
+                    <ol class="breadcrumb float-sm-end">
+                        <li class="breadcrumb-item"><a href="#">Бухгалтерия</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            Заказы
+                        </li>
+                    </ol>
+                </div>
+            </div> <!--end::Row-->
+        </div> <!--end::Container-->
+    </div> <!--end::App Content Header--> <!--begin::App Content-->
+
     {{--    <div class="container xh-100 d-flex xjustify-content-center xalign-items-center">--}}
     {{--        <div class="row w-100">--}}
     {{--            <div class="col border p-3 xtext-center w-25">--}}
-    <div class="m-2 w-25">
-        <input type="text" wire:model.live="searchTerm" placeholder="Поиск по клиенту ..."
-               class="form-control">
-        {{--                </div>--}}
-    </div>
+{{--    <div class="m-2 w-25">--}}
+{{--        <input type="text" wire:model.live="searchTerm" placeholder="Поиск по клиенту ..."--}}
+{{--               class="form-control">--}}
+{{--        --}}{{--                </div>--}}
+{{--    </div>--}}
     {{--            <div class="col border p-1 xtext-center"><span class="bg-warning p-3">{{ $searchTerm }}</span></div>--}}
     {{--            <div class="col border p-3 xtext-center"></div>--}}
     {{--        </div>--}}
